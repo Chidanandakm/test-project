73796f8f317df60c6b0fee13ec7b58c1310bcb25
chidananda <chidananda@gmail.com>
2026-05-22 13:31:26 +0530

    fixxxx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>Get started finallls</h1>
           <p>
             Edit <code>src/App.jsx</code> deployed asdf for github actions and
             save to test 51<code>HMR</code> fsssssdfasdfadsfadfadfas Lorem ipsum
